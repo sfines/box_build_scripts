e9e02244a46a72f47613800028f6f7fd0533b10b
Steven Fines <sfines@objectdriven.com>
2022-03-24 11:04:03 -0700

    (task) Add more components

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Build My Local Linux
+
+Installs:
+ 
+[Oh My Zsh](https://ohmyz.sh/)
+[Powerline 10K](https://github.com/romkatv/powerlevel10k#oh-my-zsh)
+[Powerline Fonts](https://github.com/powerline/fonts)
+
+
--- install.zsh
+++ install.zsh
@@ -3,7 +3,7 @@
 if [[ `uname` == "Darwin" ]]; then
     brew install ansible
     ansible-galaxy collection install community.general
-elif [[`uname` == "Ubuntu"]]; then
+elif [[`uname` == "Debian"]]; then
     sudo apt update
     sudo apt-add-repository --yes --update ppa:ansible/ansible
     sudo apt install ansible git unzip
